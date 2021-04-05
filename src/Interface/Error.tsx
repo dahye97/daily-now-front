985a49b5bebb353c0d998a21856abaf654957ffc
dahye97 <oon1e97w@gmail.com>
2021-04-05 10:21:45 +0900

    Add error state in registration form

---
+++ src/Interface/Error.tsx
@@ -0,0 +1,5 @@
+export interface errorInfo {
+	email: string,
+     username: string,
+     password: string,
+}
--- src/Pages/Auth/Registration.tsx
+++ src/Pages/Auth/Registration.tsx
@@ -1,14 +1,15 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { Container,FormControl,InputLabel,Input,FormHelperText,Button } from "@material-ui/core";
 import logo from '../../asset/img/logo.webp'
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 import { useCookies} from 'react-cookie';
 import { useHistory } from "react-router";
+import {errorInfo} from '../../Interface/Error';
 
 // TODO: 회원가입 페이지
 const useStyles = makeStyles({
      authContainer: {
-          padding: "100px"
+          padding: "100px",
      },
      authBox: {
 		padding: "20px",
@@ -16,14 +17,16 @@ const useStyles = makeStyles({
 		background: "#ffffff",
 		boxShadow: "13px 13px 34px #b1b1b1, -13px -13px 34px #ffffff",
 		overflow: "hidden",
-
           textAlign: "center",
 	},
      registerForm : {
-
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
      },
      input: {
-          margin: "8px"
+          margin: "8px",
+          maxWidth: "195px"
      },
      button : {
           marginTop: "20px"
@@ -38,6 +41,11 @@ export default function Registration() {
      const [email, setEmail] = useState("")
      const [password, setPassword] = useState("")
 
+     const [error, setError] = useState<errorInfo | undefined>(Object);
+
+     useEffect(() => {
+          console.log(error)
+     }, [error])
      const history = useHistory();
      const [cookies, setCookie, removeCookie] = useCookies([]);
 
@@ -51,7 +59,7 @@ export default function Registration() {
                "first_name": firstName,
                "last_name": lastName
           }
-          
+          console.log(registerInfo)
           fetch('http://192.168.0.69:8000/api/auth/register', {
                method: "POST",
                headers: {
@@ -64,7 +72,6 @@ export default function Registration() {
                          alert("회원가입 완료 :) 다시 로그인 해주세요.");
                          history.push("/")
                          res.json().then( data => {
-                              console.log(data)
                               setCookie(
                                        data.email, 
                                        data.auth_token
@@ -72,19 +79,10 @@ export default function Registration() {
                          })
                     }else {
                          res.json().then( data => {
-                              let target;
-                              // if(data.email) {
-                              //      target = document.getElementById('email-text');
-                              //      if(target){
-                              //           target.innerText = data.email.toString()
-                              //      }
-                              // }
-                              // if(data.password){
-                              //      target = document.getElementById('password-text');
-                              //      if(target){
-                              //           target.innerText = data.password.toString()
-                              //      }
-                              // }
+                              setError(data)
+                              for( let elem in data ){
+                                   document.getElementById(elem)?.setAttribute('error', "")
+                              }
                          }
                          )
                          alert('회원가입 실패')
@@ -96,25 +94,25 @@ export default function Registration() {
      const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
           const value = e.target.value;
           switch(e.target.id) {
-               case "firstName":
+               case "firstNameInput":
                     setFirstName(value)
                     break
-               case "lastName":
+               case "lastNameInput":
                     setLastName(value)
                     break
-               case "userId":
+               case "userIdInput":
                     setId(value)
                     break
-               case "email":
+               case "emailInput":
                     setEmail(value)
                     break
-               case "password":
+               case "passwordInput":
                     setPassword(value)
                     break
           }
      }
           return (
-               <Container className={classes.authContainer} maxWidth="sm">
+               <Container className={classes.authContainer} maxWidth="md">
                     <div className={classes.authBox}>
                          <img src={logo} width="80px"/>
                          <h2>Daily Check ✔</h2>
@@ -126,7 +124,7 @@ export default function Registration() {
                                    {/* 이름 */}
                                    <InputLabel htmlFor="firstName">First Name</InputLabel>
                                    <Input 
-                                   id="firstName"
+                                   id="firstNameInput"
                                    aria-describedby="firstName-text" 
                                    type="string" 
                                    onChange={onChange}/>
@@ -136,44 +134,50 @@ export default function Registration() {
                                    {/* 성 */}
                                    <InputLabel htmlFor="lastName">Last Name</InputLabel>
                                    <Input 
-                                   id="lastName"
+                                   id="lastNameInput"
                                    aria-describedby="lastName-text" 
                                    type="string" 
                                    onChange={onChange}/>
                                    <FormHelperText id="lastName-text">Enter your last name.</FormHelperText>
                               </FormControl>
 
-                              <FormControl className={classes.input}>
+                              <FormControl error={error && (error.username? true : undefined)} id="username"className={classes.input}>
                                    {/* 아이디 */}
                                    <InputLabel htmlFor="firstName">ID</InputLabel>
                                    <Input 
-                                   id="userId"
+                                   id="userIdInput"
                                    aria-describedby="userId-text" 
                                    type="string" 
                                    onChange={onChange}/>
-                                   <FormHelperText id="userId-text">Enter your ID.</FormHelperText>
+                                   <FormHelperText id="userId-text">
+                                        {error && (error.username? error.username : "Enter your ID")}
+                                   </FormHelperText>
                               </FormControl>
 
-                              <FormControl className={classes.input}>
+                              <FormControl error={error && (error.email? true : undefined)}id="email" className={classes.input}>
                                    {/* 이메일 */}
                                    <InputLabel htmlFor="email">Email(ID)</InputLabel>
                                    <Input 
-                                   id="email"
+                                   id="emailInput"
                                    aria-describedby="email-text" 
                                    type="email" 
                                    onChange={onChange}/>
-                                   <FormHelperText id="email-text" className="email-text">Enter your email.</FormHelperText>
+                                   <FormHelperText id="email-text" className="email-text">
+                                        {error && (error.email? error.email : "Enter your email")}
+                                   </FormHelperText>
                               </FormControl>
 
-                              <FormControl  className={classes.input}>
+                              <FormControl  error={error && (error.password? true : undefined)} id="password"  className={classes.input}>
                                    {/* 비밀번호*/}
                                    <InputLabel htmlFor="password">Password</InputLabel>
                                    <Input 
-                                   id="password" 
+                                   id="passwordInput" 
                                    aria-describedby="password-text" 
                                    type="password" 
                                    onChange={onChange}/>
-                                   <FormHelperText id="password-text" className="password-text">Enter your password.</FormHelperText>
+                                   <FormHelperText id="password-text" className="password-text">
+                                        {error && (error.password? error.password : "Enter your password")}
+                                   </FormHelperText>
                               </FormControl>
                               <div className={classes.button}>
                                    <Button type="submit" onClick={onSubmit}>함께하기</Button>
