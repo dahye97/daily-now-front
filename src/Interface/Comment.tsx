e6c22e0ac63e5fffbcf7abac8c5dde8b9a74552f
dahye97 <oon1e97w@gmail.com>
2021-04-13 11:25:50 +0900

    Add comment & Get comment list in DetailPost

---
+++ src/Interface/Comment.tsx
@@ -0,0 +1,11 @@
+
+export interface commentInfo {
+    comment_id : number,
+    user: string,
+    comment_content: string,
+    date : string,
+    like: number,
+    dislike: number,
+    post_id: number,
+    parent_comment: null
+}
--- src/Pages/Board/Components/Post/Components/DetailPost.tsx
+++ src/Pages/Board/Components/Post/Components/DetailPost.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React , {useState,useEffect} from 'react'
+import axios from 'axios';
+
 import { makeStyles } from '@material-ui/core/styles';
-
 import { detailPostInfo } from '../../../../../Interface/Post'
-
-import {Paper,IconButton,Typography } from '@material-ui/core/';
+import {Paper,IconButton,Typography,Button ,TextField,Card} from '@material-ui/core/';
 import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import { useHistory, useLocation } from 'react-router';
+import { commentInfo } from '../../../../../Interface/Comment';
+import { userInfo } from '../../../../../Interface/User';
 const useStyles = makeStyles({
      root: {
        width: '100%',
@@ -20,13 +22,22 @@ const useStyles = makeStyles({
        maxHeight: 600,
        background: "#fafafa"
      },
-     content: {
+     contentPaper: {
           padding: '30px',
+          margin: '10px'
+     },
+     commentPaper : {
+          margin: '10px',
+          padding: "10px",
+          boxShadow: 'none'
+     },
+     commentItem: {
+          borderBottom: '3px solid #fafafa'
      },
      handButton: {
           textAlign: "center",
           paddingTop: "30px",
-     }
+     },
    });
 
 interface Column {
@@ -36,19 +47,18 @@ interface Column {
      align?: 'left' | 'right';
 }
 
-   
-export default function DetailPost(props: {postInfo : detailPostInfo}) {
+interface stateType {
+     post_id : number
+}
+export default function DetailPost(props: {userObj: userInfo | null,}) {
+     const location = useLocation<stateType>()
      const classes = useStyles();
-     const {postInfo} = props;
-     const columns: Column[] = [
-          { id: 'title', align:'left',label: postInfo.title, maxWidth: 100 },
-          { id: 'visited', align:'right',label: '조회 '+postInfo.views, maxWidth: 30 },
-          { id: 'like', align:'right',label: '공감 '+postInfo.like, maxWidth: 30 },
-          { id: 'unlike', align:'right',label: '비공감 '+postInfo.dislike, maxWidth: 30 },
-          { id: 'author', align:'left',label: postInfo.user.slice(0,4) + '****',maxWidth: 30},
-          { id: 'date', align:'right', label: postInfo.date, maxWidth:100},
-     ];
-     const createRow = ( start: number, end : number,) => {
+     const history= useHistory();
+     const { userObj } = props;
+
+     // 선택한 게시물 행 데이터 만들기 
+     const [columns, setColumns] = useState<Column[]>([])
+      const createRow = ( start: number, end : number,) => {
           return (<TableRow style={{display:"table-row"}}>
                {columns.slice(start,end).map( column => {
                     return (<TableCell
@@ -62,23 +72,94 @@ export default function DetailPost(props: {postInfo : detailPostInfo}) {
                })}
           </TableRow>)
      }
+
+     // 선택한 게시물 데이터 불러오기 
+     const [detailPost, setDetailPost] = useState<detailPostInfo>(Object)
+     const [isLoading, setIsLoading] = useState(true)
+     const getDetailData = () => {
+          axios.post('http://192.168.0.69:8000/api/notice/detail_post', {
+               post_id: location.state.post_id
+          })
+          .then(res => {
+               setDetailPost(res.data)
+               setColumns ( [
+                    { id: 'title', align:'left',label: res.data.title, maxWidth: 100 },
+                    { id: 'visited', align:'right',label: '조회 '+res.data.views, maxWidth: 30 },
+                    { id: 'like', align:'right',label: '공감 '+res.data.like, maxWidth: 30 },
+                    { id: 'unlike', align:'right',label: '비공감 '+res.data.dislike, maxWidth: 30 },
+                    { id: 'author', align:'left',label: res.data.user.slice(0,4) + '****',maxWidth: 30},
+                    { id: 'date', align:'right', label: res.data.date, maxWidth:100},
+               ]);
+               setIsLoading(false)
+          })
+          .catch(function(error) {
+               console.log(error);
+          })
+     }
+
+     const [commentList, setCommentList] = useState<commentInfo[]>([])
+     const getCommentList = () => {
+          axios.post('http://192.168.0.69:8000/api/notice/comment_list', {
+               post_id: location.state.post_id
+          })
+          .then(res => {
+              setCommentList(res.data)
+              console.log(commentList)
+          })
+          .catch(function(error) {
+               console.log(error);
+          })
+     }
+
+     const [comment, setComment] = useState("")
+     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+          setComment(event.target.value)
+     }
+     const handleSubmit = () => {
+          if ( comment.length <= 3 ) {
+               alert('3자 이상 입력해주세요.');
+          } else {
+               if( userObj !== null){
+                    axios.post('http://192.168.0.69:8000/api/notice/write_comment', {
+                         post_id: location.state.post_id,
+                         comment_content : comment
+                    }, {
+                         headers : {
+                              "Authorization": "Token " + userObj.auth_token,
+                         }
+                    })
+                    .then(res => {
+                     //    setCommentList(res.data)
+                        console.log(res)
+                    })
+                    .catch(function(error) {
+                         console.log(error);
+                    })
+          }
+         }
+     }
+     useEffect(() => {
+          getDetailData()
+          getCommentList()
+     }, [])
      return (
           <Paper className={classes.root}>
                     <TableContainer className={classes.container}>
                          <Table stickyHeader aria-label="sticky table">
                               <TableHead>
-                                   {createRow(0,4)}
-                                   {createRow(4,6)}
+                                   {isLoading? '로딩중 ' : 
+                                   <>
+                                        {createRow(0,4)}
+                                        {createRow(4,6)}
+                                   </>
+                                   }
                               </TableHead>
-                              <TableBody>
-                                   
-                              </TableBody>
                          </Table>
                     </TableContainer>
 
                     {/* 글 내용 */}
-                    <Paper className={classes.content}>
-                         {postInfo.content}
+                    <Paper className={classes.contentPaper}>
+                         {detailPost.content}
 
                          {/* 공감,비공감 버튼 */}
                          <Typography component="div" className={classes.handButton}>
@@ -89,6 +170,62 @@ export default function DetailPost(props: {postInfo : detailPostInfo}) {
                                    <ThumbDownIcon />
                               </IconButton>
                          </Typography>
+                         
+                         {/* 목록 버튼 */}
+                         <Typography component="div" align="right">
+                             <Button onClick={()=> history.goBack()}>목록</Button>
+                         </Typography>
+                    </Paper>
+
+                    {/* 댓글 창 */}
+                    <Paper className={classes.commentPaper}>
+                         <h3>댓글 {commentList.length}</h3>  
+                         {/* 댓글 달기 */}  
+                         <Typography component="div">
+                              <TextField
+                                   value={comment}
+                                   id="contentField"
+                                   label="내용"
+                                   multiline
+                                   rows={3}
+                                   defaultValue="주제와 무관한 댓글, 악플은 삭제될 수 있습니다."
+                                   variant="outlined"
+                                   fullWidth
+                                   onChange={handleChange}
+                              />          
+                         </Typography>  
+                         <Typography component="div" align="right">
+                             <Button onClick={handleSubmit}>등록</Button>
+                         </Typography>
+
+                         {/* 댓글 리스트  */}
+                         {commentList.length === 0 ? 
+                         '댓글이 없습니다.' 
+                         : 
+                              <Card>
+                                   <ul style={{padding: '20px', listStyle: 'none'}}>
+                                   {commentList.map( comment => {
+                                   return (
+                                        <div className={classes.commentItem}>
+                              {/* 작성자 */}<li key={comment.comment_id}>{comment.user.slice(0,4) + "****"}</li>      
+                              {/* 내용 */}<li key={comment.comment_id}>{comment.comment_content}</li>
+                              {/* 시간 */}<li key={comment.comment_id}>{comment.date}</li>
+                              {/* 답글 */}<Button>답글</Button>
+                              {/* 공감, 비공감 */}
+                                             <Typography component="span" className={classes.handButton}>
+                                                  <IconButton aria-label="like">
+                                                       <ThumbUpAltIcon />
+                                                  </IconButton>
+                                                  <IconButton aria-label="dislike">
+                                                       <ThumbDownIcon />
+                                                  </IconButton>
+                                             </Typography>
+                                        </div>
+                                        )
+                                   })} 
+                                   </ul>
+                              </Card>       
+                         }
                     </Paper>
           </Paper>
      )
