7a32fc0ed20d474fb9257d85a1ccf0acbe929903
dahye97 <oon1e97w@gmail.com>
2021-04-21 13:35:30 +0900

    fix: findDOMNode is deprecated in StrictMode error

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import { useState } from 'react';
 import AppRouter from "./Router";
 import { MuiPickersUtilsProvider} from "@material-ui/pickers";
 import DateFnsUtils from '@date-io/date-fns'
-
+import { ThemeProvider, unstable_createMuiStrictModeTheme } from '@material-ui/core';
+const theme = unstable_createMuiStrictModeTheme();
 function App() {
 
   const [isInit, setisInit] = useState(true)
-
   return (
     <>
       {isInit? (	
-            <>
+            <ThemeProvider theme={theme}>
               <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <AppRouter/>
               </MuiPickersUtilsProvider>
-            </>
+            </ThemeProvider>
           )
           : (
             '동기화 중입니다.'
