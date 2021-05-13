180ec4cdc29b2c564402f305a30e53dba0281129
dahye97 <oon1e97w@gmail.com>
2021-05-13 14:44:27 +0900

    feat: Add ask withdrawal again popup in mypage

--- src/Pages/MyPage/MyPage.tsx
+++ src/Pages/MyPage/MyPage.tsx
@@ -2,7 +2,9 @@ import React, { useState,useEffect} from 'react'
 import { useHistory } from 'react-router';
 
 import {userInfo} from 'Interface/User'
-import { Container,makeStyles,FormControl,InputLabel,Input,FormHelperText,Button,Typography } from "@material-ui/core";
+import { Container,makeStyles,FormControl,InputLabel,Input,FormHelperText,Button,Typography,
+     Dialog ,DialogActions, DialogContent ,DialogContentText , DialogTitle } from "@material-ui/core";
+
 
 const useStyles = makeStyles({
      userContainer: {
@@ -28,6 +30,7 @@ export default function MyPage( props: MyPageProps) {
 
      const [error, setError]= useState(new Map())
 
+     const [isWithDrawal, setIsWithDrawal] = useState(false)
      const handleError = (errorId: string, message: string) => {
           if( errorId === "current_password" ) {
                setError((prev) => 
@@ -60,7 +63,7 @@ export default function MyPage( props: MyPageProps) {
                          },
                     }).then(res => {
                          if( res.ok ){               
-                              alert("탈퇴가 완료되었습니다.");
+                              alert("그동안 데일리 나우를 이용해주셔서 감사합니다, 탈퇴가 완료되었습니다.");
                               props.handleWithdrawal()
                               history.push("/")
                               window.sessionStorage.clear();
@@ -141,7 +144,27 @@ export default function MyPage( props: MyPageProps) {
 
                     {/* 회원 탈퇴 */}
                     <div>
-                         <Button id="withDrawButton" type="submit" onClick={onSubmit}>회원 탈퇴</Button>
+                         <Button type="submit" onClick={() => setIsWithDrawal(true)}>회원 탈퇴</Button>
+                         <Dialog
+                              open={isWithDrawal}
+                              aria-labelledby="alert-dialog-title"
+                              aria-describedby="alert-dialog-description"
+                              >
+                              <DialogTitle id="alert-dialog-title">회원 탈퇴</DialogTitle>
+                              <DialogContent>
+                                   <DialogContentText id="alert-dialog-description">
+                                   정말 떠나시는 건가요? 한번 더 생각해보지 않으시겠어요? 😥
+                                   </DialogContentText>
+                              </DialogContent>
+                              <DialogActions>
+                                   <Button id="withDrawButton" onClick={onSubmit} color="primary">
+                                   탈퇴하기
+                                   </Button>
+                                   <Button onClick={() => setIsWithDrawal(false)} color="primary" autoFocus>
+                                   돌아가기
+                                   </Button>
+                              </DialogActions>
+                         </Dialog>
                     </div>
 		     </Container>
           )
